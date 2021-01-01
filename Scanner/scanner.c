b691e50fd143a67f43376b97f08be5e42ef7e6fb
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-01-01 19:48:05 +0100

    Update scanner.c

--- Scanner/scanner.c
+++ Scanner/scanner.c
@@ -2,9 +2,37 @@
 #include "tokens.h"
 
 extern int yylex();
+extern void init();
+extern int getInt(char * name);
+extern void createInt(char * name, int builtIn, int value);
 
-int main(void)
+int main (int argc, char * argv[])
 {
+    init();
+
+    if (argc < 2)
+    {
+        return 1;
+    }
+    
+    FILE * fp;
+    char * filename;
+    long size;
+    char * buffer;
+
+    filename = argv[1];
+
+    fp = fopen(filename, "rb");
+    if (!fp) return 1;
+
+    fseek(fp, 0L, SEEK_END);
+    size = ftell(fp);
+    rewind(fp);
+
+    buffer = malloc((size + 1) * sizeof(* buffer));
+    fread(buffer, size, 1, fp);
+    buffer[size] = '\0';
+
     int token = yylex();
 
     while (token)
