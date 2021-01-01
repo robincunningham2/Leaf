3c8937cf8d1d82d5116de1baae43e96ac9a65d0a
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-01-01 15:07:11 +0100

    Add parens to token list

--- Scanner/tokens.h
+++ Scanner/tokens.h
@@ -7,3 +7,11 @@
 // Keywords / identifiers
 #define KEYWORD     5
 #define IDENTIFIER  6
+
+// Parens
+#define LPAR        7
+#define RPAR        8
+#define LBR         9
+#define RBR         10
+#define LSQB        11
+#define RSQB        12
