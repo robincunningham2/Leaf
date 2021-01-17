2deeb9a0c203f732a004a6cbade0a4718865e452
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-01-17 16:30:38 +0100

    Create: gettokenname function

--- src/Helper.c
+++ src/Helper.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "../h/Tokens.h"
 #define TRUE  1
 #define FALSE 0
 
@@ -6,6 +7,39 @@ extern char * yyprocess;
 extern int yylineno;
 extern char * yytext;
 extern int active;
+
+char * getTokenName(int token)
+{
+    if (token == INTEGER)       return "integer";
+    if (token == FLOAT)         return "float";
+    if (token == STRING)        return "string";
+    if (token == BOOLEAN)       return "boolean";
+    if (token == KEYWORD)       return "keyword";
+    if (token == IDENTIFIER)    return "identifier";
+    if (token == LPAR)          return "'('";
+    if (token == RPAR)          return "')'";
+    if (token == LBR)           return "'{'";
+    if (token == RBR)           return "'}'";
+    if (token == LSQB)          return "'['";
+    if (token == RSQB)          return "']'";
+    if (token == PLUS)          return "'+'";
+    if (token == MINUS)         return "'-'";
+    if (token == MULTI)         return "'*'";
+    if (token == SLASH)         return "'/'";
+    if (token == VBAR)          return "'|'";
+    if (token == AND)           return "'&'";
+    if (token == DOT)           return "'.'";
+    if (token == COMMA)         return "','";
+    if (token == COLON)         return "':'";
+    if (token == SEMICOLON)     return "';'";
+    if (token == EQUAL)         return "'='";
+    if (token == ISEQUAL)       return "'=='";
+    if (token == NOTEQUAL)      return "'!='";
+    if (token == LESS)          return "'<'";
+    if (token == GREATER)       return "'>'";
+    if (token == ERROR)         return "error";
+    if (token == NONE)          return "none";
+}
 
 void error(char * name)
 {
