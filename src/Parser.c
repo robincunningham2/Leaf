56d1b2e09c08950e3d1839106c3b795021782db3
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-01-17 16:05:44 +0100

    Update: parser script

--- src/Parser.c
+++ src/Parser.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include "../h/Tokens.h"
 
+extern char * yyprocess;
+extern int yylineno;
+extern char * yytext;
+extern void error(char * name);
+extern void warn(char * content);
+
 typedef struct Token {
     int token;
     int lineno;
     char * str;
 } Token;
 
-void handleToken(int token, int yylineno, char * yytext)
+int expected = STRING;
+
+int handleToken(int token)
 {
     Token t;
     t.token = token;
     t.lineno = yylineno;
     t.str = yytext;
+
+    if (expected != NONE && t.token != expected)
+    {
+        error("Unexpected Error");
+        printf("Expected '%d', got '%d'\n", expected, t.token);
+        return 1;
+    }
 
     // Do something
 }
