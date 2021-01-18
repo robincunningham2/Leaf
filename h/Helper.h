729995f92b353de9208b8e0f6a81b5f5f770d285
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-01-18 12:07:35 +0100

    Update: helper header file

--- h/Helper.h
+++ h/Helper.h
@@ -1,8 +1,25 @@
 
-#define TRUE        1
-#define FALSE       0
+#define TRUE                1
+#define FALSE               0
 
-#define TYPE_INT    1
-#define TYPE_FLOAT  2
-#define TYPE_STRING 3
-#define TYPE_BOOL   4
+#define TYPE_INT            1
+#define TYPE_FLOAT          2
+#define TYPE_STRING         3
+#define TYPE_BOOL           4
+
+#define ACTION_VALUE_CREATE 1
+
+typedef struct Token {
+    int token;
+    int lineno;
+    char * str;
+    char * name;
+} Token;
+
+typedef struct Value {
+    int type;
+    int intValue;
+    float floatValue;
+    char * stringValue;
+    int boolValue;
+} Value;
