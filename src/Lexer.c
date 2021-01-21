bc98e205fb37e82f855cea07d802139f6269e08f
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-01-21 14:51:15 +0100

    Remove

--- src/Lexer.c
+++ src/Lexer.c
@@ -3,123 +3,22 @@
 #include <string.h>
 #include "../h/Tokens.h"
 #include "../h/Helper.h"
-#include "../h/Version.h"
 
 #define yy_size_t size_t
 
 typedef struct yy_buffer_state * YY_BUFFER_STATE;
 
+extern Process Main;
+
 extern int yylex();
 extern void yy_delete_buffer(YY_BUFFER_STATE buffer);
 extern YY_BUFFER_STATE yy_scan_string(const char * str);
-extern void init();
 extern int handleToken(int token);
-extern char * yyprocess;
-extern int yylineno;
-extern char * yytext;
-extern void error(char * name);
-extern void warn(char * content);
 extern int exitProcess(int code);
-extern int startsWith(const char * pre, const char * str);
 
-const char * help = "Usage: leaf [options] [ script.lf ] [argv] \n\
-       leaf [options] \n\
-\n\
-Examples: \n\
-    leaf test.lf \n\
-    leaf test.lf -d \n\
-    leaf -v \n\
-\n\
-Options: \n\
-    -h      --help          shows this message \n\
-    -v      --version       gets Leaf version \n\
-    -d      --debug         sets System.debug variable to true";
-
-int active = FALSE, debug = FALSE, maxargv = FALSE, processargc;
-char * processargv[16];
-
-int main(int argc, char * argv[])
+int lexer(char * buffer)
 {
-    init();
-    yyprocess = argv[0];
-
-    if (argc < 2)
-    {
-        printf("%s\n", help);
-        return 0;
-    }
-
-    FILE * fp;
-    char * filename;
-    long size;
-    char * buffer;
-    YY_BUFFER_STATE buff;
-
-    for (int i = 1; i < argc; i++)
-    {
-        if (processargc < 16)
-        {
-            processargv[processargc] = arg;
-            processargc++;
-        } else if (maxargv == FALSE)
-        {
-            warn("Leaf modules can only accept 16 args at the moment.");
-            maxargv = TRUE;
-        }
-
-        char * arg = argv[i];
-        if (arg[0] == '-')
-        {
-            if (strcmp(arg, "-h") == 0
-                || strcmp(arg, "--help") == 0)
-            {
-                printf("%s\n", help);
-                return 0;
-            } else if (strcmp(arg, "-v") == 0
-                || strcmp(arg, "--version") == 0)
-            {
-                printf("v%s\n", versionString);
-                return 0;
-            } else if (strcmp(arg, "-d") == 0
-                || strcmp(arg, "--debug") == 0)
-            {
-                debug = TRUE;
-                printf("\033[37;2m");
-                printf("* Debug mode is enabled!\n");
-                printf("\033[0m");
-            }
-
-            if (i == argc - 1 && active == FALSE) return 0;
-            continue;
-        }
-
-        if (active == TRUE)
-        {
-            continue;
-        }
-
-        filename = arg;
-
-        fp = fopen(filename, "rb");
-        if (!fp)
-        {
-            error("Module Error");
-            printf("module was not found.\n");
-            return 1;
-        }
-
-        fseek(fp, 0L, SEEK_END);
-        size = ftell(fp);
-        rewind(fp);
-
-        buffer = malloc((size) * sizeof(* buffer));
-        fread(buffer, size, 1, fp);
-        buffer[size] = '\0';
-
-        buff = yy_scan_string(buffer);
-        yyprocess = argv[i];
-        active = TRUE;
-    }
+    YY_BUFFER_STATE buff = yy_scan_string(buffer);
 
     int token, code;
     token = yylex();
