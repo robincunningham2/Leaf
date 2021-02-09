37c07fc286d65ea36feed0f18cbc4334c7820006
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-02-09 16:26:49 +0100

    Update helper header

--- h/Helper.h
+++ h/Helper.h
@@ -1,35 +1,50 @@
 
-#define TRUE                1
-#define FALSE               0
+#define TRUE    1
+#define FALSE   0
 
-#define TYPE_INT            1
-#define TYPE_FLOAT          2
-#define TYPE_STRING         3
-#define TYPE_BOOL           4
+#define none    0
+#define error   -1
 
-#define ACTION_VALUE_CREATE 1
+#define token_float     1
+#define token_int       2
+#define token_bool      3
+#define token_string    4
 
-typedef struct Token {
-    int token;
-    int lineno;
-    char * str;
-    char * name;
-} Token;
+#define token_keyword   5
+#define token_id        6
 
-typedef struct Value {
-    int statusCode;
+#define token_lpar      7
+#define token_rpar      8
+#define token_lsqb      9
+#define token_rsqb      10
+
+#define token_plus      11
+#define token_minus     12
+#define token_multi     13
+#define token_slash     14
+
+#define token_vbar      15
+#define token_and       16
+#define token_dot       17
+#define token_comma     18
+#define token_colon     19
+#define token_semicolon 20
+
+#define token_equal     21
+#define token_isequal   22
+#define token_notequal  23
+#define token_less      24
+#define token_greater   25
+
+typedef struct {
+    int _success;
     int type;
-    int intValue;
-    float floatValue;
-    char * stringValue;
-    int boolValue;
-} Value;
+    int lineno;
+    char * text;
+} token_t;
 
-typedef struct Process {
-    char * version;
-    char * module;
-    char ** argv;
-    int argc;
-    char * filepath;
-    char * absolute;
-} Process;
+char * getTokenName(token_t token);
+char * concat(char * s1, char * s2, char * s3);
+
+int error(char * type, char * content);
+void warn(char * content);
